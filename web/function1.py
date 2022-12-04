32475659eb665e4dbbe9565afe1385b6f1d96ff1
Syed Mohamed <ashrafsyed.k@gmail.com>
2022-12-04 18:12:47 +0530

    Add new repository

---
+++ web/function1.py
@@ -0,0 +1,15 @@
+
+import json
+
+import os
+
+def lambda_handler(event, context):
+    print ("Hello World")
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps(list_of_dicts, default=str),
+        'headers': {
+            "Content-Type": "application/json"
+        }
+    }
