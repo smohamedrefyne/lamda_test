45fc3d4a023358b52a4b614ac59eb7dc0ce9c10c
Syedashraf89 <119793422+Syedashraf89@users.noreply.github.com>
2022-12-06 11:39:36 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 
 import os
 os.system("git diff --dirstat=files,0 HEAD~1 | sed 's/^[ 0-9.]\+% //g'")
-os.system("aws lambda list-functions --region us-south-1")
+os.system("aws lambda get-function --function-name mobile")
 print ("working")
