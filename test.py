b29ebe3441b18e176ab03120e71bbca8bae8dea8
Syedashraf89 <119793422+Syedashraf89@users.noreply.github.com>
2022-12-06 12:07:19 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 
 import os
 os.system("git diff --dirstat=files,0 HEAD~1 | sed 's/^[ 0-9.]\+% //g'")
-os.system("aws lambda get-function --function-name mobile")
+os.system("aws lambda get-function --function-name web")
 print ("working")
