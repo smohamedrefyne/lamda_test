f0f372b7722cc05856c3d47ae7d2461eaded4634
Syed Mohamed <ashrafsyed.k@gmail.com>
2022-12-06 21:35:21 +0530

    test

--- test.py
+++ test.py
@@ -3,7 +3,8 @@ import os
 import subprocess
 
 output = os.popen("git diff --dirstat=files,0 HEAD~1 | sed 's/^[ 0-9.]\+% //g'").read()
-branch = os.popen('git branch --show-current').read()
+branch = os.popen('git branch').read()
+print (branch)
 output = output.split("\n")
 output.remove('')
 print (output)
