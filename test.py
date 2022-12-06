f51ad87c1bce4dcf78bb9706cef29c4237329222
Syed Mohamed <ashrafsyed.k@gmail.com>
2022-12-06 18:30:48 +0530

    test

--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 import os
 import subprocess
 
-output = subprocess.check_output("git diff --dirstat=files,0 HEAD~1 | sed 's/^[ 0-9.]\+% //g'", shell=True)
+output = os.popen("git diff --dirstat=files,0 HEAD~1 | sed 's/^[ 0-9.]\+% //g'").read()
 for dir in output.split('/'):
   print (dir)
   os.system("aws lambda get-function --function-name %s")  (dir)
