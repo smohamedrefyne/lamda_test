5ccce42bcea756aab32481db04723ec4faebf49e
root <root@DESKTOP-KCNVMSL.localdomain>
2022-12-06 20:41:56 +0530

    test22

--- test.py
+++ test.py
@@ -4,8 +4,12 @@ import subprocess
 
 output = os.popen("git diff --dirstat=files,0 HEAD~1 | sed 's/^[ 0-9.]\+% //g'").read()
 output = output.split("\n")
+output.remove('')
 print (output)
 for dir in output:
-  print (dir)
-  #lambda_cmd = 'aws lambda get-function --function-name %s' % (dir)
-  #os.system(lambda_cmd) 
+  dir = dir.replace('/', '')
+  zip_dir = 'zip -r temp.zip %s' % (dir)
+  os.system(zip_dir)
+  lambda_cmd = 'aws lambda update-function-code --function-name %s --zip-file "fileb://temp.zip"' % (dir)
+  os.system(lambda_cmd) 
+  os.system('rm -rf temp.zip')
